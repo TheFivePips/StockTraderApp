9c3dd5a47798f036e67247b7a93793a718451211
Sam Gardner <samgardner20@gmail.com>
2023-03-02 13:21:50 -0500

    Got the articles to be sent to my number. Possibly could be flagged as spam by twilio however. Done for now

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -1,57 +1,67 @@
 import requests
+from twilio.rest import Client
 from dotenv import dotenv_values
 from datetime import datetime, timedelta
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-url = "https://www.alphavantage.co/query?"
 config = dotenv_values(".env")
+
+VIRTUAL_TWILIO_NUMBER = config["TWILIO_NUMBER"]
+VERIFIED_NUMBER = config["MY_PHONE_NUMBER"]
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = config["ALPHA_API_KEY"]
+NEWS_API_KEY = config["NEWS_API_KEY"]
+TWILIO_SID = config["TWILIO_SSID"]
+TWILIO_AUTH_TOKEN = config["TWILIO_TOKEN"]
+
 yesterday = datetime.now() - timedelta(1)
 day_before_yesterday = datetime.now() - timedelta(2)
 yesterdays_date = datetime.strftime(yesterday, '%Y-%m-%d')
 day_before_yesterdays_date = datetime.strftime(day_before_yesterday, '%Y-%m-%d')
 
-# print(yesterdays_date)
-
-
+#Get yesterday's closing stock price
 stock_params = {
     "function": "TIME_SERIES_INTRADAY",
-    "symbol": STOCK,
+    "symbol": STOCK_NAME,
     "interval": "30min",
-    "apikey": config["API_KEY"]
+    "apikey": STOCK_API_KEY,
 }
 
-response = requests.get(url, params=stock_params)
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()
+# print(data)
 
-yesterdays_close_price = data["Time Series (30min)"][f"{yesterdays_date} 16:00:00"]["4. close"]
-day_before_yesterdays_close_price = data["Time Series (30min)"][f"{day_before_yesterdays_date} 16:00:00"]["4. close"]
+yesterdays_close_price = data['Time Series (30min)'][f"{yesterdays_date} 16:00:00"]["4. close"]
+
+#Get the day before yesterday's closing stock price
+day_before_yesterdays_close_price = data['Time Series (30min)'][f"{day_before_yesterdays_date} 16:00:00"]["4. close"]
 
 difference = abs(float(yesterdays_close_price) - float(day_before_yesterdays_close_price))
-
+# calc % difference
 diff_percent = (difference / float(yesterdays_close_price)) * 100
-print(diff_percent)
 
-if diff_percent > 1:
-    news_url = "https://newsapi.org/v2/everything?"
+if abs(diff_percent) > 1:
     news_params = {
-        "apiKey": config["NEWS_API_KEY"],
+        "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
-    news_response = requests.get(news_url, params=news_params)
-    news_data = news_response.json()["articles"]
-    top_three_articles = news_data[:3]
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
 
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    three_articles = articles[:3]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in three_articles:
+        # print(article)
+        message = client.messages.create(
+            body=article["title"],
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
